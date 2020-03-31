ce4afc3f905697abd8037823796a8108d6fcc7aa
victor <victor.hurtado02@usc.edu.co>
2020-03-31 12:15:25 -0500

    backend con perros, gatos y especies

--- backend/src/models/animal.js
+++ backend/src/models/animal.js
@@ -12,6 +12,16 @@ const dogSchema= new Schema({
     raza: { type: String,  required:true}, 
 });
 
+const catSchema= new Schema({
+    name: String,
+    fur: {type: String,required: true},
+    maintenance: {type: Number,required: true},
+    age: Number,
+    personality:[String],
+    raza: { type: String,  required:true}, 
+});
+
 module.exports={
    dog: new model("Dog",dogSchema),
-} ;
+   cat: new model("Cat",catSchema),
+};
--- backend/src/routes/animals.js
+++ backend/src/routes/animals.js
@@ -4,7 +4,8 @@ const animals=require("../models/animal");
 
 //animales
 const dogModel= animals.dog;
-//rutas
+const catModel= animals.cat;
+//rutas para animales
 // perros
 router.get('/api/extractDogs',async(req,res)=>{
     const totalDogs= await dogModel.find();
@@ -22,5 +23,55 @@ router.post('/api/generateDog',async(req,res)=>{
            mensaje: "no se logro generar el perro",
        });
    }
+});
+router.delete('/api/deleteDog/:id',async(req,res)=>{
+    const _id= req.params.id;
+    try {
+        const dogdelete = await dogModel.findByIdAndDelete({_id});
+        if(!dogdelete){
+            return res.json({message:"no se encontro el id",error});
+        }
+        res.json({message:"Se elimino el perro con exito",dogdelete});
+        
+    } catch (error) {
+        return res.json({
+            message: 'ocurrio un error',
+            error
+        });
+    }
+});
+// Gatos
+router.get('/api/extractCats',async(req,res)=>{
+    const totalCats= await catModel.find();
+    res.json({mensaje:"se extrageron con exito todos los gatos",totalCats});
+});
+router.post('/api/generateCat',async(req,res)=>{
+   
+    const body = req.body;
+   try {
+    const newCat= await catModel.create(body);
+    res.status(200).json(newCat);
+    
+   } catch (error) {
+       return res.status(500).json({
+           mensaje: "no se logro generar el gato",
+       });
+   }
+});
+router.delete('/api/deleteCat/:id',async(req,res)=>{
+    const _id= req.params.id;
+    try {
+        const catdelete = await catModel.findByIdAndDelete({_id});
+        if(!catdelete){
+            return res.json({message:"no se encontro el id",error});
+        }
+        res.json({message:"Se elimino la el gato con exito",catdelete});
+        
+    } catch (error) {
+        return res.json({
+            message: 'ocurrio un error',
+            error
+        });
+    }
 });
 module.exports=router;
